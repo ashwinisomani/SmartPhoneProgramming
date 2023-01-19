11c07a7a7a6ae747bd5ef7974d11fbf2adb3a1a1
ashwinisomani <ashwini.somanisap5@gmail.com>
2023-01-19 13:37:46 -0800

    Assignment 1

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ashwini Somani on 1/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Label: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
