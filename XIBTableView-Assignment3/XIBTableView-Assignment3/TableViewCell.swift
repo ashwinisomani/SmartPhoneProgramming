64fcf4a3b604abfe87c2a7b8b9e3484a1d764009
ashwinisomani <ashwini.somanisap5@gmail.com>
2023-02-22 20:06:20 -0800

    Assignment 3

---
+++ XIBTableView-Assignment3/XIBTableView-Assignment3/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  XIBTableView-Assignment3
+//
+//  Created by Ashwini Somani on 2/21/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageContainer: UIImageView!
+    
+    @IBOutlet weak var lblImage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
